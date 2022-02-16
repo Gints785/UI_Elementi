df3158edea63ea8a73c28867911e83d91f311972
Kanders <gints.kanders22@gmail.com>
2022-02-16 14:15:03 +0200

    Istenotas darbibas ar tekstu

---
+++ Assets/Skripti/TekstaParadisana.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TekstaParadisana : MonoBehaviour {
+	//Uzglabas tekstu, kurs panemnts no input lauka
+	public string teksts;
+	// lauks no, kura es tekstu panemsu
+	public GameObject ievadesLauks;
+	//Teksta objekts, kur attelot mainiga teksta saturu
+	public GameObject tekstaAttelasanasLauks;
+
+	public void darbibasArTeksu(){
+		teksts = ievadesLauks.GetComponent<Text>().text;
+		tekstaAttelasanasLauks.GetComponent<Text>().text ="Sveiks "+teksts.ToUpper()+"!";
+	}
+}
