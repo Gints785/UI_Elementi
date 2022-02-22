f831562f26a9c6d4170843bb385dce2c08e5c8b4
Kanders <gints.kanders22@gmail.com>
2022-02-22 15:24:23 +0200

    pabeigts

---
+++ Assets/Skripti/Kursors_uz_attela.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kursors_uz_attela : MonoBehaviour {
+	public AudioSource skanasAvots;
+	public AudioClip skanaKoAtskanot;
+
+	public void uzbiditsUzAttela(){
+		skanasAvots.PlayOneShot (skanaKoAtskanot);
+	}
+	public void nobiditsNoAttela(){
+
+		skanasAvots.Stop();
+
+
+	}
+
+}
---
+++ Assets/Skripti/Objekti.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+using UnityEngine.EventSystems;
+
+public class Objekti : MonoBehaviour,IPointerDownHandler,IBeginDragHandler,
+IDragHandler,IEndDragHandler {
+	private RectTransform transformacijasLogs;
+	public Canvas kanva;
+
+
+	void Start () {
+		transformacijasLogs = GetComponent<RectTransform> ();
+	}
+	public void OnPointerDown(PointerEventData notikums){
+		Debug.Log ("Kreisais kliksis uz objekta!");
+	}
+	public void OnBeginDrag(PointerEventData notikums){
+		Debug.Log ("Uzsakta objekta vilksana!");
+	}
+	public void OnDrag(PointerEventData notikums){
+		transformacijasLogs.anchoredPosition += notikums.delta / kanva.scaleFactor;
+	}
+	public void OnEndDrag(PointerEventData notikums){
+		Debug.Log ("objekta vilksana partraukta!");
+	}
+
+}
